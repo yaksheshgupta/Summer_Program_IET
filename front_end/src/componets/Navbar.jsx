0e20cdf4c813ae42563bd34d41adb4ee668ae481
Yakshesh Gupta <yaksheshgupta59@gmail.com>
2022-08-03 00:31:37 +0530

    site

--- front_end/src/componets/Navbar.jsx
+++ front_end/src/componets/Navbar.jsx
@@ -16,10 +16,18 @@ export default function nav(){
             </div>
             <div className="dropdown" style={{marginLeft:60}}>
             <button className="dropbtn">About Us</button>
-
+            <div className="dropdown-content" style={{minWidth: 190}}>
+                    <a href="https://github.com/yaksheshgupta/Summer_Program_IET">This Repo</a>
+            </div>
             </div>
             <div className="dropdown" style={{marginLeft:70}}>
             <button className="dropbtn">Contact Us</button>
+            <div className="dropdown-content" style={{minWidth: 190}}>
+                    <a href="https://www.linkedin.com/in/yakshesh-gupta-254643230/">LinkedIn</a>
+                    <a href="https://twitter.com/GuptaYakshesh">Twitter</a>
+                    <a href="https://github.com/yaksheshgupta">GitHub</a>
+                    <a href="https://www.instagram.com/yakshesh_g/">Instagram</a>
+            </div>
             </div>
                    
             <div className="dropdown" style={{marginLeft:100}}>
