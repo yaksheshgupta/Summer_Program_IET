7046b883fcd87819129f73759d943c2a1dbd67e6
Yakshesh Gupta <yaksheshgupta59@gmail.com>
2022-08-03 01:47:20 +0530

    site2

--- front_end/src/App.jsx
+++ front_end/src/App.jsx
@@ -3,10 +3,12 @@ import Navbar from './componets/Navbar'
 import Footer from './componets/Footer'
 import Check from './componets/check'
 import About from './componets/about'
+import Front from './componets/first_part';
 function App() {
   return (
     <>
       <Navbar />
+      <Front/>
       <Check />
       <About/>
       <Footer />
---
+++ front_end/src/componets/first_part.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+export default function Front(){
+    
+    return(
+        <div className="first">
+            <div className="first_heading" style={{fontSize: 70,paddingTop: 80}}>
+                Welcome to HANGOUT
+                <div  style={{fontSize:30}}>
+                A DiFun DApp!!!
+                </div>
+            </div>
+                <div  className="first_text" style={{fontSize:30}}>
+                    This is a enjoyment website where you can listen to music,play music and read books
+                </div>
+            
+        </div>
+    )
+}
